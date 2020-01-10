4870c34fbb784f1ae9c7e41e24b5ee629190ca6c
7Bryssk <bruninho.carvalho159@gmail.com>
2020-01-09 21:34:19 -0300

    How use nodejs to do a server
    using pure nodeJS
    start using express to do a server

---
+++ casadocodigo/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log(`Servidor rodando na porta 3000`)
+})
+
+app.get('/', (req, resp) => {
+    resp.send(`<html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body>
+            </html>`)
+});
+
+/* nodejs puro
+const http = require('http')
+const servidor = http.createServer((require, response) => {
+
+    let callback
+
+    if (require.url == '/') {
+        callback = `<html>
+                        <head>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1> Casa do Código </h1>
+                        </body>
+                    </html>`
+    } else if (require.url == '/livros') {
+        callback = `<html>
+                        <head>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1> Listagem livros </h1>
+                        </body>
+                    </html>`
+    } else {
+        callback = `<html>
+                        <head>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1> 404 Not Found </h1>
+                        </body>
+                    </html>`
+    }
+
+    response.end(callback);
+});
+servidor.listen(3000);
+*/
